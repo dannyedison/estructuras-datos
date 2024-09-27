c1f8e3c93ca96b76e637301f5f4bd765eb00012b
dannyedison <dannyedison@hotmail.com>
2024-09-26 20:16:58 -0500

    Implementación de un recorrido a un arreglo de números

--- scripts.js
+++ scripts.js
@@ -1,9 +1,19 @@
-let numero = 23
-let variasCosas = ["hola", 3, true,[],function(){}, numero] //acepta todo tipo de datos
-let otroArreglo = [] //puede ser vacio
+// //Arreglos
+// let numero = 23
+// let variasCosas = ["hola", 3, true,[],function(){}, numero] //acepta todo tipo de datos
+// let otroArreglo = [] //puede ser vacio
 
-console.log(variasCosas[2])
-console.log(variasCosas[5])
-console.log(variasCosas[1] * variasCosas[5])
+// console.log(variasCosas[2])
+// console.log(variasCosas[5])
+// console.log(variasCosas[1] * variasCosas[5])
 
-console.log(otroArreglo)
+// console.log(otroArreglo)
+
+let numeros = [1,2,3,4,5,6,7,8,9,10]
+for(index = 0; index < numeros.length; index++){
+    //console.log(numeros[index])
+    if(numeros[index] % 2 == 0){
+        console.log(numeros[index])
+    }
+
+}
